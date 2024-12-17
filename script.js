3e59a445d267a38f4289a420b826f86b478ee864
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2024-12-17 12:52:19 +1100

    Implement core game logic in console w/o win condition

--- script.js
+++ script.js
@@ -1,1 +1,91 @@
-console.log("Test");
+function Gameboard() {
+  const rows = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < rows; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const placeMark = (row, column, player) => {
+    if (board[row][column].isMarked()) return;
+
+    board[row][column].addMark(player);
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, placeMark, printBoard };
+}
+
+function Cell() {
+  let value = 0;
+
+  const addMark = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  const isMarked = () => getValue() != 0;
+
+  return { addMark, getValue, isMarked };
+}
+
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = Gameboard();
+
+  const players = [
+    {
+      name: playerOneName,
+      mark: "X",
+    },
+    {
+      name: playerTwoName,
+      mark: "O",
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (row, column) => {
+    console.log(
+      `Placing ${
+        getActivePlayer().name
+      }'s mark into row ${row} & column ${column}...`
+    );
+    board.placeMark(row, column, getActivePlayer().mark);
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return { playRound, getActivePlayer };
+}
+
+const game = GameController();
